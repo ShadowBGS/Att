6b1271f76eb151fe20692f2767444e9bc84076b0
Oluwademilade Fajolu <demiladefajolu@gmail.com>
2025-09-25 02:39:34 +0000

    i tried joining using the qr but it said 404

--- src/app/join/[sessionId]/success/page.tsx
+++
@@ -1,7 +0,0 @@
-import { StudentSuccess } from "@/components/student-success"
-
-export default function SuccessPage({ params }: { params: { lecturerId: string; sessionId: string } }) {
-    return <StudentSuccess />
-}
-
-    
